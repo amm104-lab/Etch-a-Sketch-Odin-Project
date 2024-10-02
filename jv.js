330d6d8a96207b6b47c3ee1073323438a9f933c6
amm <amm@tutao.de>
2024-10-02 08:21:12 +0200

    Cleaned up code

--- jv.js
+++ jv.js
@@ -1,35 +1,38 @@
 const con = document.querySelector("#container");
 const btn = document.querySelector("button");
 const rango = document.querySelector(".ranCo");
-const opa = document.querySelector(".opaDown");
 let randon = false;
-let onion = false;
 
 function colorChoice(){
+
     var tiles = document.querySelectorAll("#tile");
+
         tiles.forEach(tile => {
+
             if(randon===true){
-            tile.addEventListener('mouseenter', () => {
-                tile.style.backgroundColor = 
-                `#${genRanHex(6)}`
-            });}
+
+                tile.addEventListener('mouseenter', () => {
+                    tile.style.backgroundColor = 
+                    `#${genRanHex(6)}`
+                })
+            }
+
             else{
+
                 tile.addEventListener('mouseenter', () => {
                     tile.style.backgroundColor = 
                     `black`
-                });
+                })
             }
     
-        });
+        })
 }
 
 rango.addEventListener("click", () => {
+
     randon = !randon;
     colorChoice();
-                })
 
-opa.addEventListener("click", () => {
-    onion = !onion;
 })
 
 function createGrid(numOfDiv){
@@ -46,6 +49,7 @@ function createGrid(numOfDiv){
                 divo.appendChild(divi);
             }
     }
+
     colorChoice();
 
 }
@@ -54,27 +58,33 @@ createGrid(16);
 
 function deleteChild(){
 
-let e = document.querySelector("#container");
+    let e = document.querySelector("#container");
+    let child = e.lastElementChild;
 
-let child = e.lastElementChild;
-while (child) {
-    e.removeChild(child);
-    child = e.lastElementChild;
-}}
+    while (child) {
+        
+        e.removeChild(child);
+        child = e.lastElementChild;
+
+    }
+}
 
 function blank(){
+
     var tiles = document.querySelectorAll("#tile");
+
     tiles.forEach(tile => {
-        tile.style.backgroundColor = '';
-    });
-    
+        tile.style.backgroundColor = ''
+    })
 }
 
 function resize(){
+
     let size = Number(prompt("Choose size of the canvas"));
+    
     deleteChild();
     createGrid(size);
+
 }
 
 const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-
